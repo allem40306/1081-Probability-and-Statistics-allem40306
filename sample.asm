f56c54cecc4ef6de0c56f9580fed0fa41c5b1902
allem40306 <allem40306@gmail.com>
2018-12-28 10:10:09 +0800

    add sample

---
+++ sample.asm
@@ -0,0 +1,13 @@
+.386
+
+include \masm32\include\Irvine32.inc
+includelib \masm32\lib\Irvine32.lib 
+includelib \masm32\lib\Kernel32.lib
+includelib \masm32\lib\User32.lib
+
+
+.data 
+.code 
+main PROC 
+main ENDP 
+END main
