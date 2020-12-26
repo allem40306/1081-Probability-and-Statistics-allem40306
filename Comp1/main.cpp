165427d8599cfeffa0b80628de858f782fefc823
allem40306 <allem40306@gmail.com>
2020-12-26 15:08:05 +0800

    update function

--- Comp1/main.cpp
+++ Comp1/main.cpp
@@ -63,7 +63,8 @@ private:
   double f4(double *arr) {
     double ret = 0;
     FOR(i, 0, D - 1) {
-      ret += 100 * (arr[i] * arr[i] - arr[i + 1]) * (arr[i] * arr[i] - arr[i + 1]) +
+      ret += 100 * (arr[i] * arr[i] - arr[i + 1]) *
+                 (arr[i] * arr[i] - arr[i + 1]) +
              (arr[i] - 1) * (arr[i] - 1);
     }
     return ret;
@@ -81,27 +82,65 @@ private:
   }
 
   double f6(double *arr) {
-    double ret = 0;
+    double ret = 0, tmp;
+    tmp = 0;
+    FOR(i, 0, D) { tmp += arr[i] * arr[i]; }
+    tmp /= (double)D;
+    tmp = -0.2 * sqrt(tmp);
+    tmp = -20 * exp(tmp);
+    ret += tmp;
+    tmp = 0;
+    FOR(i, 0, D) { tmp += cos(2 * PI * arr[i]); }
+    tmp /= (double)D;
+    tmp = exp(tmp);
+    ret -= tmp;
+    ret += 20 + exp(1);
     return ret;
   }
 
   double f7(double *arr) {
-    double ret = 0;
+    double ret = 0, tmp, tmp2;
+    tmp = 0;
+    FOR(i, 0, D) { tmp += arr[i] * arr[i]; }
+    tmp = sqrt(sqrt(abs(tmp - (double)D)));
+    ret += tmp;
+    tmp = tmp2 = 0;
+    FOR(i, 0, D) { tmp += arr[i] * arr[i]; }
+    FOR(i, 0, D) { tmp2 += arr[i]; }
+    ret += (0.5 * tmp + tmp2) / (double)D + 0.5;
     return ret;
   }
 
   double f8(double *arr) {
-    double ret = 0;
+    double ret = 0, tmp;
+    ret += 1000000 * arr[0] * arr[0];
+    tmp = 0;
+    FOR(i, 1, D) { tmp += arr[i] * arr[i]; }
+    ret += tmp;
     return ret;
   }
 
   double f9(double *arr) {
-    double ret = 0;
+    double ret = 0, tmp;
+    ret += 418.9829 * (double)D;
+    tmp = 0;
+    FOR(i, 1, D) { tmp += sin(sqrt(abs(arr[i]))); }
+    ret -= tmp;
     return ret;
   }
 
   double f10(double *arr) {
-    double ret = 0;
+    double ret = 0, tmp;
+    tmp = 0;
+    FOR(i, 1, D) {
+      FOR(j, 0, 20) {
+        tmp += pow(0.5, j) * cos(2 * pow(3, j) * PI * (arr[i] + 0.5));
+      }
+    }
+    ret += tmp;
+    tmp = 0;
+    FOR(j, 0, 20) { tmp += pow(0.5, j) * cos(pow(3, j) * PI); }
+    ret -= (double)D * tmp;
     return ret;
   }
 
@@ -183,7 +222,7 @@ void Individual::evaluate_fitness() {
 }
 
 void Individual::print_chromosome() {
-  FOR(i, 0, D) { cout << chromosome[i] << " \n"[i == D - 1]; }
+  FOR(i, 0, D) { cout << chromosome[i] << ",\n"[i == D - 1]; }
 }
 
 bool Individual::operator<(const Individual &rhs) const {
@@ -235,12 +274,15 @@ void mutation(Individual &individual) // Random resetting
   return;
 }
 
-// vector<double> res(G);
+vector<double> res(30);
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     cout << "too few argument!\n";
     return 0;
   }
+  string filename(argv[1]);
+  filename = "406261597_" + filename + ".csv";
+  freopen(filename.c_str(), "a", stdout);
   int problem_idx = stoi(argv[1]);
   problem.set_function(problem_idx);
   FOR(ti, 0, 30) {
@@ -280,7 +322,9 @@ int main(int argc, char *argv[]) {
       population.resize(D);
       sort(population.begin(), population.end());
     }
-    cout << population[0].fitness << '\n';
-    // population[0].print_chromosome();
+    res[ti] = population[0].fitness;
+    population[0].print_chromosome();
   }
+  freopen("406261597.csv", "a", stdout);
+  FOR(ti, 0, 30) { cout << fixed << res[ti] << ",\n"[ti == 30 - 1]; }
 }
